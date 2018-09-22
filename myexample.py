af77c30dbbacd59a0993016ce199100539c46817
cjewell47 <charles.jewell47@gmail.com>
2018-09-22 12:05:05 +0100

    playing around with strings some more

--- myexample.py
+++ myexample.py
@@ -5,3 +5,7 @@ my_taxes = (my_income - 100) * my_taxrate
 print(my_taxes)
 a = 'abcdefghij'
 print(a[0:3])
+print(a[::2])
+print(a[1::2])
+print(a[::-1])
+print('My name is {a} {b} {c}'.format(a='Don', b='Chucho', c='Salamanca'))
